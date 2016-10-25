f891c08c8cb3b3389a4567648af8e214b28fc951
Ag Ibragimov <agzam.ibragimov@gmail.com>
2016-10-24 23:28:08 -0700

    Removed DIRAC dev-tools due to lags and stupid messages

--- src/cljs/sample_routing/core.cljs
+++ src/cljs/sample_routing/core.cljs
@@ -2,31 +2,18 @@
   (:require [om.next :as om :refer-macros [defui]]
             [om.dom :as dom]
             [compassus.core :as c]
-            [bidi.bidi :as bidi]
-            [pushy.core :as pushy]
             [sample-routing.colors :as colors]
             [sample-routing.numbers :refer [Numbers]]
             [sample-routing.menu :refer [Menu]]
             [sample-routing.parser :as parser]
             [cognitect.transit :as transit]
-            [taoensso.timbre :as log]
-            [dirac.runtime]))
+            [taoensso.timbre :as log]))
 
 (defonce app-state
   (atom {:menu-items [{:id 0 :title "colors" :route :route.colors/list}
                       {:id 1 :title "numbers" :route :route.numbers}]}))
 
-(defonce bidi-routes
-  ["/" {""        :colors/list
-        "numbers" :numbers
-        ["colors/" :id] :colors/color}])
-
 (declare app)
-
-(defonce history
-  (pushy/pushy (fn [{:keys [handler route-params]}]
-                 (c/set-route! app handler {:params {:route-params route-params}}))
-    (partial bidi/match-route bidi-routes)))
 
 (defn send [{:keys [remote] :as env} cb]
   (let [xhr          (new js/XMLHttpRequest)
@@ -52,20 +39,19 @@
   {:next (merge state novelty)})
 
 (defonce app
-  (c/application {:routes          {:route.colors/list  colors/Colors
-                                    :route.numbers      Numbers
-                                    :route.colors/color colors/ColorDetails}
-                  :index-route     :route.colors/list
-                  :reconciler (om/reconciler
-                                {:state   app-state
-                                 :parser  (c/parser {:read parser/readf :mutate parser/mutate})
-                                 :send    send
-                                 :merge   merge-fn
-                                 :remotes [:remote]
-                                 ;; :shared  {:history history}
-                                 })
-                  :mixins          [(c/wrap-render Menu)]
-                  
+  (c/application {:routes      {:route.colors/list  colors/Colors
+                                :route.numbers      Numbers
+                                :route.colors/color colors/ColorDetails}
+                  :index-route :route.colors/list
+                  :reconciler  (om/reconciler
+                                 {:state   app-state
+                                  :parser  (c/parser {:read parser/readf :mutate parser/mutate})
+                                  :send    send
+                                  :merge   merge-fn
+                                  :remotes [:remote]
+                                  ;; :shared  {:history history}
+                                  })
+                  :mixins      [(c/wrap-render Menu)]
                   ;; :history         {:setup    #(pushy/start! history)
                   ;;                   :teardown #(pushy/stop! history)}
                   }))
@@ -73,7 +59,6 @@
 (defonce mounted? (atom false))
 
 (defn init []
-  (dirac.runtime/install!)
   (enable-console-print!)
   (if-not @mounted?
     (do
